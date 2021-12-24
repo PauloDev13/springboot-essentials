7bfa8f02e821793744ccd03631f9e6be7f0a3031
PauloDev13 <devpgm2020@gmail.com>
2021-12-23 23:59:06 -0300

    Adding save method

--- src/main/java/com/prmorais/springboot/service/AnimeService.java
+++ src/main/java/com/prmorais/springboot/service/AnimeService.java
@@ -5,11 +5,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
-  private final List<Anime> animes = List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk"));
+  private static final List<Anime> animes;
+
+  static {
+    animes = new ArrayList<>(
+        List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk"))
+    );
+  }
+
   public List<Anime> list() {
     return animes;
   }
@@ -19,5 +28,11 @@ public class AnimeService {
         .findFirst()
         .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,
             String.format("Anime com ID %s not found", id)));
+  }
+
+  public Anime save(Anime anime) {
+    anime.setId(ThreadLocalRandom.current().nextLong(3, 1000));
+    animes.add(anime);
+    return anime;
   }
 }
